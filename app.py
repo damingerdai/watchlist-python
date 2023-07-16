7de497751b7881a50f11c545af5cc69633bb8e70
Arthur Ming <guobin.ming@pwc.com>
2023-07-16 14:36:03 +0800

    feat: add template view

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
 # import connexion
-from flask import Flask
+from re import escape
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-print(app)
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 @app.route('/')
+@app.route('/home')
+@app.route('/index')
 def hello():
-    return 'welcome to My Watchlist!'
+    return render_template('index.html', name=name, movies=movies)
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='greyli'))
+    print(url_for('user_page', name='peter'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2)) 
+    return '<div>Hello, this is test page</div>'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3000)
+    app.run(host='127.0.0.1', port=5000)
